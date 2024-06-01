d6311fe43df6d65dd2d7f44a60ed247e876aa07d
VitroidFPV <vitroid05@gmail.com>
2024-06-02 00:52:48 +0200

    Feature: Use markdown linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,20 @@
 import js from "@eslint/js"
+import markdownPlugin from "eslint-plugin-markdownlint"
+import markdownParser from "eslint-plugin-markdownlint/parser.js"
 
 export default [
 	js.configs.recommended,
 	{
-		ignores: ["node_modules/", ".vitepress/cache/"]
-	}
+		ignores: ["node_modules/", ".env*", "pnpm-lock.yaml", "package-lock.json", "yarn.lock", ".vitepress/cache/"],
+		plugins: {
+			markdownlint: markdownPlugin,
+		},
+		rules: {
+			...markdownPlugin.configs.recommended.rules,
+		},
+		languageOptions: {
+			parser: markdownParser,
+		},
+		files: ["**/*.md"]
+	},
 ]
